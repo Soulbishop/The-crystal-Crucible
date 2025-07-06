8e96621aa6fdb77cae9482f34eec3762f4933b05
Soulbishop <86578127+Soulbishop@users.noreply.github.com>
2025-07-05 23:41:23 -0400

    Update TouchInputService.java

--- app/src/main/java/com/screenmirror/samsung/service/TouchInputService.java
+++ app/src/main/java/com/screenmirror/samsung/service/TouchInputService.java
@@ -11,11 +11,10 @@ import android.view.accessibility.AccessibilityEvent;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-// This service implements the TouchCallback interface from StreamingService to receive touch events.
 public class TouchInputService extends AccessibilityService implements StreamingService.TouchCallback {
 
     private static final String TAG = "TouchInputService";
-    private Handler mainHandler; // Handler for dispatching gestures on the main thread
+    private Handler mainHandler;
 
     @Override
     public void onCreate() {
@@ -28,7 +27,6 @@ public class TouchInputService extends AccessibilityService implements Streaming
     protected void onServiceConnected() {
         super.onServiceConnected();
         Log.d(TAG, "TouchInputService connected.");
-        // Register this service as the TouchCallback for StreamingService
         StreamingService streamingService = StreamingService.getInstance();
         if (streamingService != null) {
             streamingService.setTouchCallback(this);
@@ -52,37 +50,27 @@ public class TouchInputService extends AccessibilityService implements Streaming
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "TouchInputService disconnected.");
-        // Unregister this service as the TouchCallback for StreamingService
         StreamingService streamingService = StreamingService.getInstance();
         if (streamingService != null) {
-            streamingService.setTouchCallback(null); // Unregister
+            streamingService.setTouchCallback(null);
             Log.d(TAG, "TouchInputService unregistered from StreamingService.");
         }
     }
 
-    // Implementation of StreamingService.TouchCallback
     @Override
     public void onTouchEvent(float x, float y, String action) {
         Log.d(TAG, "Received touch event via callback: x=" + x + ", y=" + y + ", action=" + action);
 
-        // Dispatch gestures on the main thread
         mainHandler.post(() -> {
             Path path = new Path();
             path.moveTo(x, y);
 
             GestureDescription.Builder builder = new GestureDescription.Builder();
-            // Action "down" corresponds to a new stroke
             if ("down".equals(action)) {
-                builder.addStroke(new GestureDescription.StrokeDescription(path, 0, 1)); // Duration 1ms for tap
+                builder.addStroke(new GestureDescription.StrokeDescription(path, 0, 1));
             } else if ("move".equals(action)) {
-                // For move, typically you'd need to continue an existing stroke.
-                // This requires more complex state management of ongoing gestures.
-                // For simplicity, we'll treat moves as single points for now, or you'd track stroke IDs.
                 builder.addStroke(new GestureDescription.StrokeDescription(path, 0, 1));
             } else if ("up".equals(action)) {
-                // For "up", you'd end an existing stroke.
-                // Similar to "move", requires state management.
-                // For now, it will be treated as a release point.
                 builder.addStroke(new GestureDescription.StrokeDescription(path, 0, 1));
             }
 
